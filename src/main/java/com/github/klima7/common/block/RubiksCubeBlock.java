1d8019d340b3418e014ac8149ec30db046d5f9e4
Lukasz Klimkiewicz <ukasz.klimkiewicz@gmail.com>
2022-08-24 01:16:57 +0200

    Fix RC rotation bug

--- src/main/java/com/github/klima7/common/block/RubiksCubeBlock.java
+++ src/main/java/com/github/klima7/common/block/RubiksCubeBlock.java
@@ -15,12 +15,13 @@ import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class RubiksCubeBlock extends Block implements EntityBlock {
+public class RubiksCubeBlock extends DirectionalBlock implements EntityBlock {
 
     public static final Properties PROPERTIES = BlockBehaviour.Properties
             .of(Material.WOOD)
@@ -43,10 +44,14 @@ public class RubiksCubeBlock extends Block implements EntityBlock {
         return RenderShape.ENTITYBLOCK_ANIMATED;
     }
 
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
     @javax.annotation.Nullable
     @Override
     public BlockState getStateForPlacement(@NotNull BlockPlaceContext context) {
-        return this.defaultBlockState();
+        return this.defaultBlockState().setValue(FACING, context.getNearestLookingDirection().getOpposite());
     }
 
     @Nullable
