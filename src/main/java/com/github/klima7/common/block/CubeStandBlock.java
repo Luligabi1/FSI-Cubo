082224f3c91b972c0fef8d41d875a5d9f6f6a4b4
klima7 <ukasz.klimkiewicz@gmail.com>
2023-02-07 11:41:25 +0100

    Add detecting clicks to stand

--- src/main/java/com/github/klima7/common/block/CubeStandBlock.java
+++ src/main/java/com/github/klima7/common/block/CubeStandBlock.java
@@ -1,7 +1,12 @@
 package com.github.klima7.common.block;
 
+import com.github.klima7.common.item.BaseRubiksCubeItem;
 import com.github.klima7.core.init.BlockEntityRegistry;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
@@ -12,6 +17,7 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Material;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.BooleanOp;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
@@ -62,4 +68,14 @@ public class CubeStandBlock extends Block implements EntityBlock {
         return SHAPE;
     }
 
+    @Override
+    public InteractionResult use(BlockState block, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult result) {
+        ItemStack item = player.getMainHandItem();
+
+        if(!(item.getItem() instanceof BaseRubiksCubeItem)) {
+            return InteractionResult.CONSUME;
+        }
+
+        return InteractionResult.SUCCESS;
+    }
 }
